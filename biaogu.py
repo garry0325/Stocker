dd85dd6ed7739df4fae4cc120c1e7a9f00a04f57
Garry Yeung <garry0325@Garrys-MacBook-Pro-Retina.local>
2019-11-17 23:22:16 +0800

    Add various filters

--- biaogu.py
+++ biaogu.py
@@ -4,6 +4,9 @@ import datetime
 from dateutil.relativedelta import relativedelta
 import stockInfo
 
+global stockDictionary
+stockDict = []
+
 class bband():
 	def __init__(self, price, MA, upper, lower):
 		self.price = price
@@ -15,15 +18,17 @@ def calculateBBands(date=datetime.datetime.now(),
 					trackbackDates=90,
 					N=20,	# days to make MA
 					K=2):	# amplitude of band in stdev
-	stockList = {}
+	
 	latestDate = stockInfo.generateStockPricesDictionaryByDate(date)['2330'].date
 	datesNeeded = trackbackDates + N - 1
 	day = 1
 	
+	stockList = {}
 	date = datetime.datetime.now()
 	while day <= datesNeeded:
 		stockPrices = stockInfo.generateStockPricesDictionaryByDate(date, autoCorrectDate=False)
 		if(stockPrices != None):
+			stockDict.append(stockPrices)
 			for stockItem in stockPrices:
 				if(stockItem in stockList):
 					stockList[stockItem] = np.append(stockList[stockItem], stockPrices[stockItem].price)
@@ -39,7 +44,6 @@ def calculateBBands(date=datetime.datetime.now(),
 		if(len(stockList[stockItem]) != datesNeeded):
 			del stockList[stockItem]
 			continue
-
 
 		bbandList[stockItem] = []
 
@@ -59,7 +63,7 @@ def plotBand(bbandList, stockId):
 	upper = []
 	lower = []
 	price = []
-
+	
 	for i in bbandList[stockId]:
 		price.insert(0, i.price)
 		ma.insert(0, i.MA)
@@ -67,11 +71,11 @@ def plotBand(bbandList, stockId):
 		lower.insert(0, i.lower)
 
 	l = range(0, len(ma))
+	plt.figure(figsize=(10, 5))
 	plt.plot(l, price, c='red', linewidth=2.0)
 	plt.plot(l, ma, linewidth=1.5, alpha=0.5)
 	plt.plot(l, upper, linewidth=1.5, alpha=0.5)
 	plt.plot(l, lower, linewidth=1.5, alpha=0.5)
-
 	plt.xlabel(stockId)
 	plt.ylabel('Price')
 
@@ -101,7 +105,51 @@ def filterHighestPriceForDays(bbandList, days=20):
 
 	print("%d stocks found after highest price filter" % (len(bbandList)))
 
+	return bbandList
+
+def filterByMAandVolume(bbandList, date, duration=5, ratio=2):
+	bbandListCopy = bbandList.copy()
+	for stockItem in bbandListCopy:
+		
+		# filter by volume
+		volume = 0
+		for i in range(0, duration):
+			volume = volume + stockDict[i][stockItem].volume
+		volume = volume / duration
+		if((stockDict[0][stockItem].volume / volume) < ratio):
+			del bbandList[stockItem]
+			continue
+
+		# filter by MA order
+		MA5 = 0
+		MA10 = 0
+		MA20 = 0
+		for i in range(0, 20):
+			if(i < 5):
+				MA5 = MA5 + stockDict[i][stockItem].price
+			if(i < 10):
+				MA10 = MA10 + stockDict[i][stockItem].price
+			if(i < 20):
+				MA20 = MA20 + stockDict[i][stockItem].price
+		if(not (MA5 < MA10 and MA10 < MA20)):
+			del bbandList[stockItem]
+			continue
+
+	print("%d stocks found after MA & volume filter" % (len(bbandList)))
+	return bbandList
+
 date = datetime.datetime(2019, 11, 16)
 bband = calculateBBands(date, trackbackDates=90)
 bband = filterPriceHigherThanUpper(bband)
 bband = filterHighestPriceForDays(bband)
+bband = filterByMAandVolume(bband, date)
+plotBand(bband, '1103')
+'''
+for i in bband:
+	print(i)
+c = input('show plot (y/n)?')
+if(c == 'y'):
+	for i in bband:
+		plotBand(bband, i)
+
+'''
